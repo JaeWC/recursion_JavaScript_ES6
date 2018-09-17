26590ef35c256c2f15ccc23c547ca06a89ae4613
Jae Choi <j.thechois@gmail.com>
2018-09-17 19:02:40 +0900

    complete getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,6 +7,27 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var bodyElement = document.body;
+  var classElement = [];
+
+  function findClassName(element) {
+
+
+  	if (element.classList && element.classList.contains(className)) {
+  		classElement.push(element);
+  	}
+
+  	if (element.hasChildNodes()) {
+  		var children = element.childNodes;
+  		for (var i = 0; i < children.length; i++) {
+  			findClassName(children[i]);
+  		}
+  	}
+  }
+
+  findClassName(bodyElement);
+
+  return classElement;
 };
 
 if ( typeof module === "object" && typeof module.exports === "object" ) {
